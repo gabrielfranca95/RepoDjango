83f8517a5a4849eed7383ca2ca41a16728d1e392
gabriel franca <gabrielfurtunatofranca@gmail.com>
2022-03-26 02:56:44 -0300

    versão 0.3

--- mysite/livro/models.py
+++ mysite/livro/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Livros(models.Model):
     nome = models.CharField(max_length = 100)
     autor = models.CharField(max_length = 30)
-    co_autor = models.CharField(max_length = 30)
+    co_autor = models.CharField(max_length = 30, blank = True, null = True)
     data_cadastro = models.DateField()
     emprestado = models.BooleanField(default=False)
     nome_emprestado = models.CharField(max_length = 30)
@@ -14,3 +14,6 @@ class Livros(models.Model):
 
     class Meta:
         verbose_name = 'Livro'
+    
+    def __str__(self):
+        return self.nome
